c75d2d0e2d7b447a2ba724e9c33873a8668af448
ahmed-elmenan <zahmajd4@gmail.com>
2022-05-13 22:58:07 +0100

    add finger print authentication

--- android/app/src/main/kotlin/com/example/swifty_protein/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/swifty_protein/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.swifty_protein
 
-import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterFragmentActivity() {
+class MainActivity: FlutterActivity() {
 }

3325161be9b7211777b6e653f3645e3013b48590
ahmed-elmenan <zahmajd4@gmail.com>
2022-05-13 22:56:33 +0100

    add finger print authenticationfgsedgdsfgds

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:local_auth/local_auth.dart';
 import 'package:swifty_protein/splash_screen.dart';
 
 void main() {
@@ -20,9 +22,9 @@ class SwiftyProtein extends StatelessWidget {
           return MaterialApp(
             debugShowCheckedModeBanner: false,
             theme: ThemeData(fontFamily: 'roboto_regular'),
-            home: const SignIn(),
+            home: SignIn(),
             routes: {
-              '/sign-in': (context) => const SignIn(),
+              '/sign-in': (context) => SignIn(),
             },
           );
         }
@@ -31,13 +33,97 @@ class SwiftyProtein extends StatelessWidget {
   }
 }
 
-class SignIn extends StatelessWidget {
-  const SignIn({Key? key}) : super(key: key);
+class SignIn extends StatefulWidget {
+  SignIn({Key? key}) : super(key: key);
+
+  @override
+  State<SignIn> createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  LocalAuthentication auth = LocalAuthentication();
+
+  bool? _canCheckBiometric;
+
+  List<BiometricType>? _availableBiometrics;
+
+  String authorized = "Not Authorized";
+
+  Future _checkBiometric() async {
+    bool? canCheckBiometric;
+    try {
+      canCheckBiometric = await auth.canCheckBiometrics;
+    } on PlatformException catch (e) {
+      print(e);
+    }
+    if (mounted) {
+      setState(() {
+        _canCheckBiometric = canCheckBiometric;
+      });
+    }
+  }
+
+  Future _getAvailableBiometrics() async {
+    List<BiometricType>? availableBiometric;
+    try {
+      availableBiometric = await auth.getAvailableBiometrics();
+    } on PlatformException catch (e) {
+      print(e);
+    }
+    setState(() {
+      _availableBiometrics = availableBiometric;
+    });
+  }
+
+  Future _authenticate() async {
+    bool authenticated = false;
+    try {
+      authenticated = await auth.authenticate(
+          localizedReason: "scan your finger to authenticate",
+          options: const AuthenticationOptions(
+              useErrorDialogs: true, stickyAuth: false));
+    } on PlatformException catch (e) {
+      print(e);
+    }
+    if (mounted) {
+      setState(() {
+        authorized = authenticated ? "Authorized" : "Failed";
+        if (authenticated) {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => const Home()));
+        }
+        print(authorized);
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    _checkBiometric();
+    _getAvailableBiometrics();
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("sign in")),
+    return Scaffold(
+      body: Center(
+          child: GestureDetector(
+              onTap: () => _authenticate(), child: const Text("sign in"))),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.blue,
+        child: Text("Home"),
+      ),
     );
   }
 }
